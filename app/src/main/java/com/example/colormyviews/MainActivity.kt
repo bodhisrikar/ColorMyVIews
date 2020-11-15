16d42284ca9a29978686dceb5ac90d6323e914dd
srikar <srikar@localhost-live.hsd1.md.comcast.net>
2020-11-15 15:46:04 -0500

    adding click listeners

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainActivityBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        val clickableViews: List<View> = listOf(mainActivityBinding.boxOneText, mainActivityBinding.boxTwoText, mainActivityBinding.boxThreeText,
+            mainActivityBinding.boxFourText, mainActivityBinding.boxFiveText, mainActivityBinding.constraintLayout)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColoured(it) }
+        }
+    }
+
+    private fun makeColoured(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(resources.getColor(android.R.color.holo_green_light))
+            R.id.box_four_text -> view.setBackgroundColor(resources.getColor(android.R.color.holo_green_dark))
+            R.id.box_five_text -> view.setBackgroundColor(resources.getColor(android.R.color.holo_green_light))
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
